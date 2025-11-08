35dc199ed7ac125b7c983853444244a414fd4e4f
sye7qjm3ac <sye7qjm3ac@icloud.com>
2025-11-08 17:20:18 +0700

    Initial Commit

---
+++ DCTHash/DCTHashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCTHashApp.swift
+//  DCTHash
+//
+//  Created by Eyhciurmrn Zmpodackrl on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DCTHashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
